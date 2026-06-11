4f1907af398c7b6bab1384b3a96d45cd3f75f82c
zerobazar <zerobazar387@gmail.com>
2026-06-11 13:43:47 -0400

    Update save-reservation.js

--- netlify/functions/save-reservation.js
+++ netlify/functions/save-reservation.js
@@ -1,1 +1,33 @@
-exports.handler=async()=>({statusCode:200,body:JSON.stringify({success:true})});
+exports.handler = async (event) => {
+  try {
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbwO9TUVxMF0N9c3m2EURyp3C7iT3OeZ3tsfJURnfB8iItqYa5qjpuCyH5QxhtTf-6s/exec",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: event.body,
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        success: false,
+        error: error.message,
+      }),
+    };
+  }
+};
