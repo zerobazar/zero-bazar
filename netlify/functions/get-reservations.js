157c7a4a6df4648671452cbe8d3b21db76b296e5
zerobazar <zerobazar387@gmail.com>
2026-06-11 13:43:07 -0400

    Update get-reservations.js

--- netlify/functions/get-reservations.js
+++ netlify/functions/get-reservations.js
@@ -1,1 +1,26 @@
-exports.handler=async()=>({statusCode:200,body:JSON.stringify({ok:true})});
+exports.handler = async () => {
+  try {
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbwO9TUVxMF0N9c3m2EURyp3C7iT3OeZ3tsfJURnfB8iItqYa5qjpuCyH5QxhtTf-6s/exec"
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        success: false,
+        error: error.message,
+      }),
+    };
+  }
+};
