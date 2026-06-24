ce171f597ba8e13a89bff5991672108c8b4ef1b5
zerobazar <zerobazar387@gmail.com>
2026-06-24 11:13:05 -0400

    Update save-reservation.js

--- netlify/functions/save-reservation.js
+++ netlify/functions/save-reservation.js
@@ -3,31 +3,42 @@ exports.handler = async (event) => {
     const API_URL='/.netlify/functions/get-reservations';
 const SAVE_URL='/.netlify/functions/save-reservation';
       {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: event.body,
-      }
-    );
+     exports.handler = async (event) => {
 
-    const data = await response.json();
+ try {
 
-    return {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        success: false,
-        error: error.message,
-      }),
-    };
-  }
+  const response = await fetch(
+   "https://script.google.com/macros/s/AKfycbwO9TUVxMF0N9c3m2EURyp3C7iT3OeZ3tsfJURnfB8iItqYa5qjpuCyH5QxhtTf-6s/exec",
+   {
+    method: "POST",
+    headers: {
+     "Content-Type": "application/json"
+    },
+    body: event.body
+   }
+  );
+
+  const text = await response.text();
+
+  return {
+   statusCode: 200,
+   headers: {
+    "Access-Control-Allow-Origin": "*"
+   },
+   body: text
+  };
+
+ } catch (error) {
+
+  return {
+   statusCode: 500,
+   body: JSON.stringify({
+    success:false,
+    error:error.message
+   })
+  };
+
+ }
+
+};
 };
